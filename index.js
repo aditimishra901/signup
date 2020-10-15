200803dbb6be0535df77bb9b7015beabe70d4e49
aditimishra901 <aditimishra1261@gmail.com>
2020-10-15 17:30:56 +0530

    added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require("express");
-const cors = require('cors'); 
+const cors = require('cors');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
@@ -11,12 +11,12 @@ const personalRoute = require("./routes/personal");
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>{  // To remove CROS (cross-resource-origin-platform) problem
-    res.setHeader('Access-Control-Allow-Origin',"*"); // to allow all client we use *
-    res.setHeader('Access-Control-Allow-Methods',"OPTIONS,GET,POST,PUT,PATCH,DELETE"); //these are the allowed methods
-    res.setHeader('Access-Control-Allow-Headers', "*"); // allowed headers (Auth for extra data related to authoriaztiom)
-    next();
-})
+// app.use((req, res, next) =>{  // To remove CROS (cross-resource-origin-platform) problem
+//     res.setHeader('Access-Control-Allow-Origin',"*"); // to allow all client we use *
+//     res.setHeader('Access-Control-Allow-Methods',"OPTIONS,GET,POST,PUT,PATCH,DELETE"); //these are the allowed methods
+//     res.setHeader('Access-Control-Allow-Headers', "*"); // allowed headers (Auth for extra data related to authoriaztiom)
+//     next();
+// })
 
 mongoose.connect("mongodb+srv://test:aditi@cluster0.403sd.mongodb.net/projectDB?retryWrites=true&w=majority",{
   useNewUrlParser: true,
