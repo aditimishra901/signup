a384a0e065ca571dac24d7b3d6e4f24d93507988
aditimishra901 <aditimishra1261@gmail.com>
2020-10-15 16:47:42 +0530

    added

--- routes/auth.js
+++ routes/auth.js
@@ -85,11 +85,11 @@ router.post("/login", function (req,res) {
 
         //create and assign token
 
-        console.log("after if of bcrypt");
+        console.log(process.env.TOKEN_SECRET);
         const token = jwt.sign({_id:user._id}, process.env.TOKEN_SECRET);
-
+        // console.log("after token generation");
         res.header('auth-token',token).json({token:token, userId:user._id.toString() , message:'User logged in', username:user.name})
-console.log("after token generation");
+
    })
    .catch((err) => {
       res.json("something went wrong")
