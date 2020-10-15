061f1c8f241f37847e8b98b947bc525f36cae1d2
aditimishra901 <aditimishra1261@gmail.com>
2020-10-15 16:52:00 +0530

    added

--- routes/personal.js
+++ routes/personal.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const verify = require('./verifyToken');
 
-router.get("/home",verify,function(req,res){
+router.get("/",verify,function(req,res){
 
    res.json({personal:{title:"secret diary", secret: "shhhh it's a secret!!"}});
 
